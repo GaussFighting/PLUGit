a73760de9a4baa332ce3869e9ebbdb1e07057e5f
GaussFighting <krzysztof.kowerczyk@gmail.com>
2023-03-05 13:02:51 +0100

    folders structure, components, sass, bootstrap and reacstrap added

---
+++ src/components/Contact.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Contact = () => {
+  return <div> Contact </div>;
+};
+export default Contact;
---
+++ src/components/Offers.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Offers = () => {
+  return <div> Offers </div>;
+};
+export default Offers;
